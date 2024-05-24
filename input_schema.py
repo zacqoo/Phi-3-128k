0a6c44ea76b42c5b19ca1572a151148638343d3e
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-05-25 01:14:04 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -2,17 +2,15 @@ INPUT_SCHEMA = {
     "roles": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [-1],
-        'example': ["user", "assistant", "user"]
+        'shape': [1],
+        'example': ["user"]
     },
     "chunks": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [-1],
+        'shape': [1],
         'example': [
             "Inferless is a machine learning model deployment platform.",
-            "It simplifies the deployment process for machine learning models.",
-            "Users can deploy models without worrying about infrastructure ?"
         ]
     }
 }
