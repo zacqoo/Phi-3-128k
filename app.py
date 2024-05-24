6b08eeef0cfd5e8a65d9c9c1ea4df664375b3b89
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-05-25 01:13:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import json
-import numpy as np
-import torch
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
 class InferlessPythonModel:
     def initialize(self):
         self.model = AutoModelForCausalLM.from_pretrained(
-            "microsoft/Phi-3-mini-128k-instruct", 
-            device_map="cuda", 
-            torch_dtype="auto", 
-            trust_remote_code=True, 
+            "microsoft/Phi-3-mini-128k-instruct",
+            device_map="cuda",
+            torch_dtype="auto",
+            trust_remote_code=True,
         )
         self.tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
         self.pipe = pipeline(
@@ -17,32 +14,20 @@ class InferlessPythonModel:
             model=self.model,
             tokenizer=self.tokenizer,
         )
-    
-    def infer(self, input_data):
-        if isinstance(input_data, dict):
-            input_json = input_data
-        else:
-            input_json = json.loads(input_data)
 
-        chunks = input_json['chunks']
-        roles = input_json['roles']
+    def infer(self, input_data):
+        chunks = input_data['chunks']
+        roles = input_data['roles']
 
         generation_args = {
             "max_new_tokens": 500,
             "return_full_text": False,
             "temperature": 0.0,
-            "do_sample": False,
-        }
-
+            "do_sample": False,}
         messages = []
-
-        for i in range(len(chunks)) :
-            messages.append({ "role": roles[i] , "content" : chunks[i] })
-        
+        messages.append({ "role": roles , "content" : chunks })
         output = self.pipe(messages, **generation_args)
-
         return {"result": output[0]['generated_text'] }
-
 
     def finalize(self):
         self.generator = None
